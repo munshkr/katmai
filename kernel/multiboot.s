4804c1eec56cff21145eb8abf77f36d308bb2e1e
Damián Emiliano Silvani <munshkr@gmail.com>
2011-01-19 21:01:07 -0300

    Remove unused multiboot constants and use %define macros instead

--- kernel/multiboot.s
+++ kernel/multiboot.s
@@ -25,76 +25,31 @@
 ; http://www.gnu.org/software/grub/manual/multiboot/multiboot.html
 ;
 
-%ifndef MUTLIBOOT_HEADER
-%define MULTIBOOT_HEADER 1
-
-; How many bytes from the start of the file we search for the header.
-MULTIBOOT_SEARCH                  equ 8192
-
 ; The magic field should contain this.
-MULTIBOOT_HEADER_MAGIC            equ 0x1badb002
+%define MULTIBOOT_HEADER_MAGIC        0x1badb002
 
 ; This should be in %eax.
-MULTIBOOT_BOOTLOADER_MAGIC        equ 0x2badb002
+%define MULTIBOOT_BOOTLOADER_MAGIC    0x2badb002
 
 ; The bits in the required part of flags field we don't support.
-MULTIBOOT_UNSUPPORTED             equ 0x0000fffc
+%define MULTIBOOT_UNSUPPORTED         0x0000fffc
 
 ; Alignment of multiboot modules.
-MULTIBOOT_MOD_ALIGN               equ 0x00001000
+%define MULTIBOOT_MOD_ALIGN           0x00001000
 
 ; Alignment of the multiboot info structure.
-MULTIBOOT_INFO_ALIGN              equ 0x00000004
+%define MULTIBOOT_INFO_ALIGN          0x00000004
 
 ; Flags set in the 'flags' member of the multiboot header.
 
 ; Align all boot modules on i386 page (4KB) boundaries.
-MULTIBOOT_PAGE_ALIGN              equ 0x00000001
+%define MULTIBOOT_PAGE_ALIGN          0x00000001
 
 ; Must pass memory information to OS.
-MULTIBOOT_MEMORY_INFO             equ 0x00000002
+%define MULTIBOOT_MEMORY_INFO         0x00000002
 
 ; Must pass video information to OS.
-MULTIBOOT_VIDEO_MODE              equ 0x00000004
+%define MULTIBOOT_VIDEO_MODE          0x00000004
 
 ; This flag indicates the use of the address fields in the header.
-MULTIBOOT_AOUT_KLUDGE             equ 0x00010000
-
-; Flags to be set in the 'flags' member of the multiboot info structure.
-
-; is there basic lower/upper memory information?
-MULTIBOOT_INFO_MEMORY             equ 0x00000001
-; is there a boot device set?
-MULTIBOOT_INFO_BOOTDEV            equ 0x00000002
-; is the command-line defined?
-MULTIBOOT_INFO_CMDLINE            equ 0x00000004
-; are there modules to do something with?
-MULTIBOOT_INFO_MODS               equ 0x00000008
-
-; These next two are mutually exclusive
-
-; is there a symbol table loaded?
-MULTIBOOT_INFO_AOUT_SYMS          equ 0x00000010
-; is there an ELF section header table?
-MULTIBOOT_INFO_ELF_SHDR           equ 0x00000020
-
-; is there a full memory map?
-MULTIBOOT_INFO_MEM_MAP            equ 0x00000040
-
-; Is there drive info?
-MULTIBOOT_INFO_DRIVE_INFO         equ 0x00000080
-
-; Is there a config table?
-MULTIBOOT_INFO_CONFIG_TABLE       equ 0x00000100
-
-; Is there a boot loader name?
-MULTIBOOT_INFO_BOOT_LOADER_NAME   equ 0x00000200
-
-; Is there a APM table?
-MULTIBOOT_INFO_APM_TABLE          equ 0x00000400
-
-; Is there video information?
-MULTIBOOT_INFO_VIDEO_INFO         equ 0x00000800
-
-
-%endif ; MULTIBOOT_HEADER
+%define MULTIBOOT_AOUT_KLUDGE         0x00010000
