31e8e5c3b60f94f9b631d6ffb643408a120ebe75
Damián Emiliano Silvani <munshkr@gmail.com>
2011-01-21 23:51:21 -0300

    Add function for remapping 8259 PICs and set gates for the 16 IRQs

--- kernel/descriptor_tables.c
+++ kernel/descriptor_tables.c
@@ -23,6 +23,7 @@
 
 
 static void idt_set_gate(uint8_t index, uint32_t offset, uint16_t selector, uint8_t flags);
+static void remap_pics(uint32_t offset1, uint32_t offset2);
 
 
 const uint32_t CODE_SEGMENT    = 0x08;
@@ -36,56 +37,114 @@ idt_ptr_t   idt_ptr;
 
 void init_idt()
 {
-     idt_ptr.limit = sizeof(idt_entry_t) * 256 - 1;
-     idt_ptr.base  = (uint32_t) &idt_entries;
+    idt_ptr.limit = sizeof(idt_entry_t) * 256 - 1;
+    idt_ptr.base  = (uint32_t) &idt_entries;
 
-     memset((uint32_t*) &idt_entries, 0, sizeof(idt_entry_t) * 256);
+    memset((uint32_t*) &idt_entries, 0, sizeof(idt_entry_t) * 256);
 
-     // TODO ? Put ISR functions inside an array and make this a for loop
-     idt_set_gate( 0, (uint32_t) isr0 , CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate( 1, (uint32_t) isr1 , CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate( 2, (uint32_t) isr2 , CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate( 3, (uint32_t) isr3 , CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate( 4, (uint32_t) isr4 , CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate( 5, (uint32_t) isr5 , CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate( 6, (uint32_t) isr6 , CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate( 7, (uint32_t) isr7 , CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate( 8, (uint32_t) isr8 , CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate( 9, (uint32_t) isr9 , CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(10, (uint32_t) isr10, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(11, (uint32_t) isr11, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(12, (uint32_t) isr12, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(13, (uint32_t) isr13, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(14, (uint32_t) isr14, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(15, (uint32_t) isr15, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(16, (uint32_t) isr16, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(17, (uint32_t) isr17, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(18, (uint32_t) isr18, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(19, (uint32_t) isr19, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(20, (uint32_t) isr20, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(21, (uint32_t) isr21, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(22, (uint32_t) isr22, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(23, (uint32_t) isr23, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(24, (uint32_t) isr24, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(25, (uint32_t) isr25, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(26, (uint32_t) isr26, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(27, (uint32_t) isr27, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(28, (uint32_t) isr28, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(29, (uint32_t) isr29, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(30, (uint32_t) isr30, CODE_SEGMENT, INT_GATE_FLAGS);
-     idt_set_gate(31, (uint32_t) isr31, CODE_SEGMENT, INT_GATE_FLAGS);
+    // TODO See if we can put ISR functions inside an array
+    // and make this a for loop.
+    idt_set_gate( 0, (uint32_t) isr0 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate( 1, (uint32_t) isr1 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate( 2, (uint32_t) isr2 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate( 3, (uint32_t) isr3 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate( 4, (uint32_t) isr4 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate( 5, (uint32_t) isr5 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate( 6, (uint32_t) isr6 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate( 7, (uint32_t) isr7 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate( 8, (uint32_t) isr8 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate( 9, (uint32_t) isr9 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(10, (uint32_t) isr10, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(11, (uint32_t) isr11, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(12, (uint32_t) isr12, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(13, (uint32_t) isr13, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(14, (uint32_t) isr14, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(15, (uint32_t) isr15, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(16, (uint32_t) isr16, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(17, (uint32_t) isr17, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(18, (uint32_t) isr18, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(19, (uint32_t) isr19, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(20, (uint32_t) isr20, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(21, (uint32_t) isr21, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(22, (uint32_t) isr22, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(23, (uint32_t) isr23, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(24, (uint32_t) isr24, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(25, (uint32_t) isr25, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(26, (uint32_t) isr26, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(27, (uint32_t) isr27, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(28, (uint32_t) isr28, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(29, (uint32_t) isr29, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(30, (uint32_t) isr30, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(31, (uint32_t) isr31, CODE_SEGMENT, INT_GATE_FLAGS);
 
-     load_idt((void *) &idt_ptr);
+    // There are 2 PICs (Master and Slave) and both have DATA and COMMAND
+    // ports.  At initialization, their IRQs are mapped to interrupts
+    // 0x08-0x0F and 0x70-0x77 respectively.  Because Master mapping
+    // conflicts with the CPU exception interrupts in protected mode,
+    // the Master PIC needs to be remapped.
+
+    // We set them to 0x20 (i.e. 32, just after IA-32 exceptions) and 0x70
+    // (same as before for slave PIC).
+    remap_pics(0x20, 0x70);
+
+    idt_set_gate(32, (uint32_t) irq0 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(33, (uint32_t) irq1 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(34, (uint32_t) irq2 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(35, (uint32_t) irq3 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(36, (uint32_t) irq4 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(37, (uint32_t) irq5 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(38, (uint32_t) irq6 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(39, (uint32_t) irq7 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(40, (uint32_t) irq8 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(41, (uint32_t) irq9 , CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(42, (uint32_t) irq10, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(43, (uint32_t) irq11, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(44, (uint32_t) irq12, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(45, (uint32_t) irq13, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(46, (uint32_t) irq14, CODE_SEGMENT, INT_GATE_FLAGS);
+    idt_set_gate(47, (uint32_t) irq15, CODE_SEGMENT, INT_GATE_FLAGS);
+
+    // Finally load the IDT base address in the LIDT register
+    load_idt((void *) &idt_ptr);
 }
+
 
 static void idt_set_gate(uint8_t index, uint32_t offset, uint16_t selector, uint8_t flags)
 {
-     idt_entries[index].offset_lo = offset & 0xFFFF;
-     idt_entries[index].offset_hi = (offset >> 16) & 0xFFFF;
+    idt_entries[index].offset_lo = offset & 0xFFFF;
+    idt_entries[index].offset_hi = (offset >> 16) & 0xFFFF;
 
-     idt_entries[index].selector = selector;
-     idt_entries[index].reserved = 0;
-     // We must uncomment the OR below when we get to using user-mode.
-     // It sets the interrupt gate's privilege level to 3.
-     idt_entries[index].flags = flags /* | 0x60 */;
+    idt_entries[index].selector = selector;
+    idt_entries[index].reserved = 0;
+
+    // We must uncomment the OR below when we get to using user-mode.
+    // It sets the interrupt gate's privilege level to 3.
+    idt_entries[index].flags = flags /* | 0x60 */;
+}
+
+static void remap_pics(uint32_t offset1, uint32_t offset2)
+{
+    // Save IRQ masks
+    uint8_t a1 = inb(PIC1_DATA);
+    uint8_t a2 = inb(PIC2_DATA);
+
+    // Start the initialization sequence:
+    outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+    outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+
+    // Send the offset vectors (they must be multiple of 8)
+    outb(PIC1_DATA, offset1);
+    outb(PIC2_DATA, offset2);
+
+    // TODO Figure out what do these bytes mean...
+    outb(PIC1_DATA, 4);
+    outb(PIC2_DATA, 2);
+
+    // Set 8086/88 mode
+    outb(PIC1_DATA, ICW4_8086);
+    outb(PIC2_DATA, ICW4_8086);
+
+    // Restore saved masks
+    outb(PIC1_DATA, a1);
+    outb(PIC2_DATA, a2);
 }
