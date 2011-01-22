41f857159a79d65830b83f83a5aea37af88ca0db
Damián Emiliano Silvani <munshkr@gmail.com>
2011-01-22 10:34:28 -0300

    Support variable arguments list

---
+++ kernel/stdarg.h
@@ -0,0 +1,29 @@
+/*
+* stdarg.h ~ Variable Arguments List (GCC-only)
+*
+* Copyright 2010 Damián Emiliano Silvani <dsilvani@gmail.com>,
+*                Patricio Reboratti <darthpolly@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+// Use GCC built-in functionality for variable arguments
+
+#define va_start(v,l) __builtin_va_start(v,l)
+#define va_arg(v,l)   __builtin_va_arg(v,l)
+#define va_end(v)     __builtin_va_end(v)
+#define va_copy(d,s)  __builtin_va_copy(d,s)
+
+typedef __builtin_va_list va_list;
